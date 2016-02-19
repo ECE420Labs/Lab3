a806e83cca00c78da3ef8f49ea116be44d2f4223
Andrea McIntosh <akmcinto@ualberta.ca>
2016-02-19 00:41:59 -0700

    Made a faster solution?

--- old_sol.c
+++ old_sol.c
@@ -44,16 +44,15 @@ int main (int argc, char* argv[]) {
     if (size == 1)
         X[0] = Au[0][1] / Au[0][0];
     else{
-        //#pragma omp parallel
         {
         /*Gaussian elimination*/
         Gauss_elim(thread_count_);
         /*printf("%f ", Au[0][0]);printf("%f ", Au[0][1]);printf("%f\n", Au[0][2]);
         printf("%f ", Au[1][0]);printf("%f ", Au[1][1]);printf("%f\n", Au[1][2]);
         printf("%f ", Au[2][0]);printf("%f ", Au[2][1]);printf("%f\n", Au[2][2]);*/
-        //Jordan_elim(thread_count_); // serial
+        Jordan_elim(thread_count_); // serial
 
-        //solve(thread_count_);
+        solve(thread_count_);
         //printf("%f ", X[0]);printf("%f ", X[1]);printf("%f\n", X[2]);
         }
 
@@ -68,52 +67,32 @@ printf("%f\n", end-start);
 
 void Gauss_elim(int nt) {
     int k;
-    int j=0;
-    int i=0;
-    double temp=0;
-    #pragma omp parallel num_threads(nt)
-    {
-    #pragma omp single
+    int j;
+    int i;
+    double temp;
     for (k = 0; k < size - 1; ++k){
         /*Pivoting*/
         temp = 0;
         j = 0;
-        {
-            for (i = k; i < size; ++i) {// Find row with largest kth element
-                if (temp < Au[ind[i]][k] * Au[ind[i]][k]){ // square value to make it positive
-                    temp = Au[ind[i]][k] * Au[ind[i]][k];
-                    j = i; // j is the row ind with the largest element for column k
-                }
-            }
-
-            if (j != k)/*swap*/{
-                i = ind[j];
-                ind[j] = ind[k];
-                ind[k] = i;
+        for (i = k; i < size; ++i) {// Find row with largest kth element
+            if (temp < Au[ind[i]][k] * Au[ind[i]][k]){ // square value to make it positive
+                temp = Au[ind[i]][k] * Au[ind[i]][k];
+                j = i; // j is the row ind with the largest element for column k
             }
         }
+        if (j != k)/*swap*/{
+            i = ind[j];
+            ind[j] = ind[k];
+            ind[k] = i;
+        }
         /*calculating*/
-        //#pragma omp for private(j, temp)
+        #pragma omp parallel for num_threads(nt) private(j, temp)
         for (i = k + 1; i < size; ++i){
             temp = Au[ind[i]][k] / Au[ind[k]][k];
             for (j = k; j < size + 1; ++j) {
                 Au[ind[i]][j] -= Au[ind[k]][j] * temp;
             }
         }
-    }
-    //#pragma omp for ordered private(temp, i)
-    #pragma omp single
-    for (k = size - 1; k > 0; --k){
-        for (i = k - 1; i >= 0; --i ){
-            temp = Au[ind[i]][k] / Au[ind[k]][k];
-            Au[ind[i]][k] -= temp * Au[ind[k]][k];
-            Au[ind[i]][size] -= temp * Au[ind[k]][size]; // output vector
-        }
-    }
-    #pragma omp for
-    for (k=0; k< size; ++k) {
-        X[k] = Au[ind[k]][size] / Au[ind[k]][k];
-    }
     }
 }
 
@@ -123,11 +102,21 @@ void Jordan_elim(int nt) {
     int i;
     double temp;
 
-
+    for (k = size - 1; k > 0; --k){
+        #pragma omp parallel for num_threads(nt) private(temp)
+        for (i = k - 1; i >= 0; --i ){
+            temp = Au[ind[i]][k] / Au[ind[k]][k];
+            Au[ind[i]][k] -= temp * Au[ind[k]][k];
+            Au[ind[i]][size] -= temp * Au[ind[k]][size]; // output vector
+        }
+    }
 }
 
 void solve(int nt) {
     /*solution*/
     int k;
-
+    #pragma omp parallel for num_threads(nt)
+    for (k=0; k< size; ++k) {
+        X[k] = Au[ind[k]][size] / Au[ind[k]][k];
+    }
 }
